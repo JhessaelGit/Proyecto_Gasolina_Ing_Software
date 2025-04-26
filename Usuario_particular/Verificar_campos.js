dcadf67413b67f06019509553b8958ebd2d37a55
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-04-26 19:00:41 -0400

    Refactor: las funciones se separaron par que hagan solo lo que deben hacer

--- Usuario_particular/Verificar_campos.js
+++ Usuario_particular/Verificar_campos.js
@@ -1,9 +1,11 @@
 function El_string_no_esta_vacio(cadena){
     const el_string_esta_vacio = cadena === "" || cadena === undefined
-    
     let el_string_es_valido = !el_string_esta_vacio
-    
     return el_string_es_valido
+}
+
+function verificar_nombre(nombre_usuario){
+    return El_string_no_esta_vacio(nombre_usuario)
 }
 
 function El_string_solo_tiene_numeros(cadena){
@@ -22,4 +24,4 @@ function verificar_ci(ci_usuario)
     return el_ci_es_valido
 }
 
-export {El_string_no_esta_vacio, verificar_ci}
+export {verificar_nombre, verificar_ci}
--- Usuario_particular/Verificar_campos.test.js
+++ Usuario_particular/Verificar_campos.test.js
@@ -1,14 +1,14 @@
-import {El_string_no_esta_vacio,verificar_ci} from "./Verificar_campos";
+import {verificar_nombre,verificar_ci} from "./Verificar_campos";
 
 describe("Pruebas para verificar el nombre",() => {
     it("Si el campo del nombre es una cadena vacia deberia retornar false", () => {
-        expect(El_string_no_esta_vacio("")).toEqual(false)
+        expect(verificar_nombre("")).toEqual(false)
     })
     it("Si el campo del nombre esta con nombre deveria devolver true", () => {
-        expect(El_string_no_esta_vacio("Jhessael")).toEqual(true)
+        expect(verificar_nombre("Jhessael")).toEqual(true)
     })
     it("Si el campo del nombre esta vacio deveria devolver un false", () => {
-        expect(El_string_no_esta_vacio()).toEqual(false)
+        expect(verificar_nombre()).toEqual(false)
     })
 })
 
