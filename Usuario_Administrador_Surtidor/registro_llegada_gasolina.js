04bae4106d8f19e4d46dcb4257fcff11351620b4
Tairin-Munoz <tairinmunoz14@gmail.com>
2025-04-29 10:27:33 -0400

    se corrigio una parte del archivo

--- Usuario_Administrador_Surtidor/registro_llegada_gasolina.js
+++ Usuario_Administrador_Surtidor/registro_llegada_gasolina.js
@@ -9,7 +9,6 @@ boton_registrar_llegada_gasolina.addEventListener("click", (event) => {
 
   const todos_los_campos_son_validos = verificar_cantidad_gasolina(cantidad_gasolina_value);
   if (todos_los_campos_son_validos){
-    window.location.href = "Usuario_administrador_surtidor";
   }
   else{
     alert("Por favor, ingrese los datos de llegada de gasolina correctamente")
