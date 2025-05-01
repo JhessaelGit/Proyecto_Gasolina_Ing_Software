1696c2c318c980c80761b2cde1220cb23e248a3a
Diego Rios <diegoarv08@hotmail.com>
2025-05-01 14:57:03 -0400

    Revert "test(surtidores): agregar prueba unitaria cuando son las 8am"
    This reverts commit f9f3e74ab9f766be3861f111ae11712fae5a87c2.

--- Surtidores/surtidores.test.js
+++ Surtidores/surtidores.test.js
@@ -12,7 +12,4 @@ describe("Pruebas para verificar el cambio de hora",() => {
     it("Si la hora pasada las 10pm (22pm) deberia retornar false", () => {
         expect(actualizarEstadoHorario(23)).toEqual(false)
     })
-    it("Si la hora es 8am deberia retornar false", () => {
-        expect(actualizarEstadoHorario(23)).toEqual(false)
-    })
 })
