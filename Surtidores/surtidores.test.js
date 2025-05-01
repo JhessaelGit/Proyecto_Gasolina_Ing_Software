034b0c617840717378bbba3ff034188ed64d02eb
Diego Rios <diegoarv08@hotmail.com>
2025-05-01 15:07:49 -0400

    test(surtidores): agregar prueba unitaria cuando son las 8am

--- Surtidores/surtidores.test.js
+++ Surtidores/surtidores.test.js
@@ -3,6 +3,9 @@ describe("Pruebas para verificar el cambio de hora",() => {
     it("Si la hora es antes de las 8am deberia retornar false", () => {
         expect(actualizarEstadoHorario(7)).toEqual(false)
     })
+    it("Si la hora es 8am deberia retornar true", () => {
+        expect(actualizarEstadoHorario(8)).toEqual(true)
+    })
     it("Si la hora es entre 8am y 10pm (22pm) deberia retornar true", () => {
         expect(actualizarEstadoHorario(15)).toEqual(true)
     })
