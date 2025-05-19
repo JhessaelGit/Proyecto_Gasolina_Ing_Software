8e0c09e695fc741bfaee1d177e9b633f419d7abb
melanytorrez <torrezmelany13@gmail.com>
2025-05-18 23:29:17 -0400

    test: decrementar contador en 1

--- Surtidores/contador.js
+++ Surtidores/contador.js
@@ -2,5 +2,8 @@ function incrementarContador(valorActual) {
   return valorActual + 1;
 }
 
+function decrementarContador(valorActual) {
+  return Math.max(0, valorActual - 1);
+}
 
-export { incrementarContador };
+export { incrementarContador, decrementarContador };
--- Surtidores/contador.test.js
+++ Surtidores/contador.test.js
@@ -1,8 +1,13 @@
-import { incrementarContador } from './contador.js';
+import { incrementarContador, decrementarContador } from './contador.js';
 
 describe("Lógica del contador de autos en fila", () => {
   it("Debe incrementar en 1", () => {
     expect(incrementarContador(0)).toBe(1);
     expect(incrementarContador(5)).toBe(6);
+  });
+
+  it("Debe decrementar en 1 si es mayor que 0", () => {
+    expect(decrementarContador(3)).toBe(2);
+    expect(decrementarContador(1)).toBe(0);
   });
 });
