4df2dd6ae83047728ff2b5c3ef912316e535c538
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-05-19 14:22:06 -0400

    Feat: cuando el usuario ingresa un ticket Existente se le muestra el mensaje 'se cancelo correctamente el ticket'

---
+++ Usuario_Administrador_Surtidor/Cancelar_ticket.js
@@ -0,0 +1,12 @@
+const boton_cancelar=document.querySelector("#cancelar-button")
+const codigo_ticket= document.querySelector("#codigo-ticket")
+
+boton_cancelar.addEventListener("click",(event)=>{
+    event.preventDefault();
+    const codigo_ticket_value=codigo_ticket.value;
+    if(codigo_ticket_value === "TK-LJFK5C" && "TK-1PVC3G")
+    {
+        alert("El Ticket fue cancelado Exitosamente");
+    }
+    
+})
