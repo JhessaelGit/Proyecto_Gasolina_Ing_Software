ba7ffee7302f7070f1a800b7ffe84851013d263e
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-05-25 17:43:12 -0400

    Feat<Registro llegada gasolina>: Ahora actualiza la cantidad de gasolina

--- Usuario_Administrador_Surtidor/registro_llegada_gasolina.js
+++ Usuario_Administrador_Surtidor/registro_llegada_gasolina.js
@@ -1,18 +1,19 @@
 import { verificar_cantidad_gasolina } from "../Verificar_datos_registro/Verificar_campos";
+
 const cantidad_gasolina = document.querySelector("#cantidad-gasolina");
-const boton_registrar_llegada_gasolina= document.querySelector("#registrar-llegada-gasolina-button")
-const formulario_registrar_llegada_gasolina = document.querySelector("#registrar-llegada-gasolina-form")
+const boton_registrar_llegada_gasolina = document.querySelector("#registrar-llegada-gasolina-button");
+const formulario_registrar_llegada_gasolina = document.querySelector("#registrar-llegada-gasolina-form");
+const gasolinaDisponibleElemento = document.querySelector("#gasolina-disponible");
 
 boton_registrar_llegada_gasolina.addEventListener("click", (event) => {
   event.preventDefault();
   const cantidad_gasolina_value = cantidad_gasolina.value;
 
   const todos_los_campos_son_validos = verificar_cantidad_gasolina(cantidad_gasolina_value);
-  if (todos_los_campos_son_validos){
-    alert("Registro De Gasolina realizado")
+  if (todos_los_campos_son_validos) {
+    gasolinaDisponibleElemento.textContent = cantidad_gasolina_value;
+    alert("Registro de gasolina realizado correctamente");
+  } else {
+    alert("Por favor, ingrese los datos de llegada de gasolina correctamente");
   }
-  else{
-    alert("Por favor, ingrese los datos de llegada de gasolina correctamente")
-  }
-
 });
