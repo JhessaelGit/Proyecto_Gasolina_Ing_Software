e2dc89cfc3b88715e747ca728a27c53fa8295e98
Tairin-Munoz <tairinmunoz14@gmail.com>
2025-05-25 19:27:04 -0400

    test: promedio para varios autos en fila

--- Surtidores/promedio_carga.test.js
+++ Surtidores/promedio_carga.test.js
@@ -7,5 +7,9 @@ describe("Cálculo del promedio de carga de gasolina", () => {
   it("Debe calcular correctamente para 1 auto", () => {
     expect(calcularPromedioCarga(1)).toBe(45);
   });
+  it("Debe calcular para múltiples autos", () => {
+    expect(calcularPromedioCarga(3)).toBe(135);
+    expect(calcularPromedioCarga(10)).toBe(450);
+  });
 
 });
