144a1b362b66940edb0da13eccd84fb3db3b0ed5
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-05-25 17:50:08 -0400

    Feat<Registro llegada gasolina>: Cambia el valor de la disponibilidad de la gasolina en el surtidor

--- Usuario_Administrador_Surtidor/registro_llegada_gasolina.js
+++ Usuario_Administrador_Surtidor/registro_llegada_gasolina.js
@@ -4,6 +4,7 @@ const cantidad_gasolina = document.querySelector("#cantidad-gasolina");
 const boton_registrar_llegada_gasolina = document.querySelector("#registrar-llegada-gasolina-button");
 const formulario_registrar_llegada_gasolina = document.querySelector("#registrar-llegada-gasolina-form");
 const gasolinaDisponibleElemento = document.querySelector("#gasolina-disponible");
+const estadoActualElemento = document.querySelector("#estado-actual");
 
 boton_registrar_llegada_gasolina.addEventListener("click", (event) => {
   event.preventDefault();
@@ -11,7 +12,16 @@ boton_registrar_llegada_gasolina.addEventListener("click", (event) => {
 
   const todos_los_campos_son_validos = verificar_cantidad_gasolina(cantidad_gasolina_value);
   if (todos_los_campos_son_validos) {
-    gasolinaDisponibleElemento.textContent = cantidad_gasolina_value;
+    const cantidadNumerica = parseInt(cantidad_gasolina_value);
+
+    gasolinaDisponibleElemento.textContent = cantidadNumerica;
+
+    if (cantidadNumerica < 100) {
+      estadoActualElemento.textContent = "Agotado";
+    } else {
+      estadoActualElemento.textContent = "Disponible";
+    }
+
     alert("Registro de gasolina realizado correctamente");
   } else {
     alert("Por favor, ingrese los datos de llegada de gasolina correctamente");
