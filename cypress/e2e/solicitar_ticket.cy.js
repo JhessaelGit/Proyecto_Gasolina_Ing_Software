cec285e9fe27bf696a682408dfe0601b675610ab
Diego Rios <diegoarv08@hotmail.com>
2025-05-24 21:51:08 -0400

    feat(test)<solicitar_ticket.cy.js>: Agrega el registro en fila, completando el flujo

--- cypress/e2e/solicitar_ticket.cy.js
+++ cypress/e2e/solicitar_ticket.cy.js
@@ -29,6 +29,24 @@ describe('Solicitar ticket', () => {
 
     // 8. Hacer click en el botón "Solicitar Ticket"
     cy.get('#btn-ticket').click();
-
+    
+    // Volver atrás en el navegador para seguir con el flujo
+    cy.wait(5000); // espera 5 segundo 
+    cy.go('back');
+    
+    // 9. Guardar la cantidad actual de personas en la fila
+    cy.get('#contador-fila').invoke('text').then((textBefore) => {
+        
+        const filaAntes = parseInt(textBefore);
+    
+        // 10. Hacer clic en "Ingresar a la fila"
+        cy.get('#btn-ingresar-fila').click();
+        
+        // 11. Verificar que la cantidad en la fila ha incrementado en 1
+        cy.get('#contador-fila').should(($contador) => {
+            const filaDespues = parseInt($contador.text());
+            expect(filaDespues).to.eq(filaAntes + 1);
+        });
+    });
   });
 });
