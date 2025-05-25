f08dc7159e8051b60b71b98fb694f61fa0ff20bf
Tairin-Munoz <tairinmunoz14@gmail.com>
2025-05-25 19:25:23 -0400

    refactor: promedio de 45 para 1 auto en fila

--- Surtidores/promedio_carga.js
+++ Surtidores/promedio_carga.js
@@ -1,5 +1,7 @@
-export function calcularPromedioCarga(autosEnFila) {
+const LITROS_POR_AUTO = 45;
+
+function calcularPromedioCarga(autosEnFila) {
     
-  const LITROS_POR_AUTO = 45;
   return Math.max(0, autosEnFila) * LITROS_POR_AUTO;
 }
+export { calcularPromedioCarga };
